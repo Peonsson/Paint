5c806a6b17da1b989c06f3e81292bdff4714e933
Johan Pettersson <xsnudelx@hotmail.com>
2016-04-06 22:47:20 +0200

    prettified

--- src/view/Paint.java
+++ src/view/Paint.java
@@ -9,12 +9,12 @@ import model.Shape;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  * Created by Peonsson on 2016-04-03.
  */
+
 public class Paint extends JFrame {
 
     public static void main(String[] args) {
@@ -63,6 +63,9 @@ public class Paint extends JFrame {
     private JComboBox thicknessComboBox = new JComboBox(thickness);
     private JCheckBox filledCheckBox = new JCheckBox("isFilled");
 
+    /*
+        Constructor
+     */
     public Paint() {
         menuPanel.add(saveButton);
         menuPanel.add(loadButton);
@@ -95,6 +98,59 @@ public class Paint extends JFrame {
         add(canvas, BorderLayout.CENTER);
     }
 
+    /*
+        Paint logic
+     */
+    private class myCanvas extends JPanel {
+
+        public myCanvas() { }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            for (Shape shape : shapes) {
+                int x1 = shape.getX1();
+                int x2 = shape.getX2();
+                int y1 = shape.getY1();
+                int y2 = shape.getY2();
+                boolean isFilled = shape.isFilled();
+                int thickness = shape.getThickness();
+                Color color = shape.getColor();
+
+                if (shape instanceof Line) {
+                    Graphics2D g2 = (Graphics2D) g;
+                    g2.setStroke(new BasicStroke(thickness));
+                    g.setColor(color);
+                    g.drawLine(x1, y1, x2, y2);
+                } else if (shape instanceof Rectangle) {
+                    g.setColor(color);
+                    if (isFilled) {
+                        g.fillRect(x1, y1, x2, y2);
+                    } else {
+                        Graphics2D g2 = (Graphics2D) g;
+                        g2.setStroke(new BasicStroke(thickness));
+                        g.drawRect(x1, y1, x2, y2);
+                    }
+                } else if (shape instanceof Oval) {
+                    g.setColor(color);
+                    if (isFilled) {
+                        g.fillOval(x1, y1, x2, y2);
+                    } else {
+                        Graphics2D g2 = (Graphics2D) g;
+                        g2.setStroke(new BasicStroke(thickness));
+                        g.drawOval(x1, y1, x2, y2);
+                    }
+                } else {
+                    System.err.println("unrecognised shape!");
+                }
+            }
+        }
+    }
+
+    /*
+        Getters and setters
+     */
     public ArrayList<Shape> getUndoShapes() {
         return undoShapes;
     }
@@ -189,52 +245,5 @@ public class Paint extends JFrame {
 
     public JPanel getCanvas() {
         return canvas;
-    }
-
-    private class myCanvas extends JPanel {
-
-        public myCanvas() { }
-
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-
-            for (Shape shape : shapes) {
-                int x1 = shape.getX1();
-                int x2 = shape.getX2();
-                int y1 = shape.getY1();
-                int y2 = shape.getY2();
-                boolean isFilled = shape.isFilled();
-                int thickness = shape.getThickness();
-                Color color = shape.getColor();
-
-                if (shape instanceof Line) {
-                    Graphics2D g2 = (Graphics2D) g;
-                    g2.setStroke(new BasicStroke(thickness));
-                    g.setColor(color);
-                    g.drawLine(x1, y1, x2, y2);
-                } else if (shape instanceof Rectangle) {
-                    g.setColor(color);
-                    if (isFilled) {
-                        g.fillRect(x1, y1, x2, y2);
-                    } else {
-                        Graphics2D g2 = (Graphics2D) g;
-                        g2.setStroke(new BasicStroke(thickness));
-                        g.drawRect(x1, y1, x2, y2);
-                    }
-                } else if (shape instanceof Oval) {
-                    g.setColor(color);
-                    if (isFilled) {
-                        g.fillOval(x1, y1, x2, y2);
-                    } else {
-                        Graphics2D g2 = (Graphics2D) g;
-                        g2.setStroke(new BasicStroke(thickness));
-                        g.drawOval(x1, y1, x2, y2);
-                    }
-                } else {
-                    System.err.println("unrecognised shape!");
-                }
-            }
-        }
     }
 }
