b44b672f70cb53662a26727cd2286a69e0c22347
Peonsson <xsnudelx@hotmail.com>
2016-06-08 12:34:23 +0200

    prettified

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -69,13 +69,9 @@ public class Controller {
                     int size = model.getShapes().size();
                     if(size == 0)
                         return;
-                    Shape temp = model.getShapes().get(size - 1);
 
+                    Shape temp = model.getShapes().get(size - 1);
                     model.modifyShape(temp, x, y, width, height);
-//                    temp.setX1(x);
-//                    temp.setY1(y);
-//                    temp.setX2(width);
-//                    temp.setY2(height);
                 }
             }
 
